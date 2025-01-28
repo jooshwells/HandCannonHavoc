50c20761d80faa363ffd126edfd90a5bd796688e
Joshua Wells <144137022+jooshwells@users.noreply.github.com>
2025-01-27 22:53:39 -0500

    Messing around with a player character in my scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private int jump_speed = 10  ;
+    // Start is called before the first frame update
+    
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, jump_speed / 2);
+        }
+    }
+}
