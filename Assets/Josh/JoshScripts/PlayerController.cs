85d10e0e0219c28bdb3e5a86ed6a601a747b9523
jooshwells <joshwells637@gmail.com>
2025-02-03 15:53:29 -0500

    Still working on wall jump.

--- Assets/Josh/JoshScripts/PlayerController.cs
+++ Assets/Josh/JoshScripts/PlayerController.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float wallSlide = 0.95f;
+
     private float horizontal;
     private float speed = 8f;
-    private float jumpPower = 16f;
+    public float jumpPower = 16f;
+    public float wallJumpPower = 16f;
     private bool isFacingRight = true;
+    private bool isJumping = false;
+    private bool wallJumped = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -19,32 +26,46 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        if (IsGrounded())
+        {
+            isJumping = false;
+            wallJumped = false;
+        }
+        if (!IsOnWall())
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        } else if (IsOnWall() && !wallJumped)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * wallSlide);
 
-        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                wallJumped = true;
+                if(isFacingRight) 
+                { 
+                    rb.velocity = new Vector2(rb.velocity.x * wallJumpPower, jumpPower);
+                    Debug.Log("facing right");
+                } 
+                else
+                {
+                    rb.velocity = new Vector2(-wallJumpPower, jumpPower);
+                    Debug.Log("facing LEFT");
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
         }
-
-        if(IsOnWall() &&  !IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 2);
-        }
-
-
-        if(IsOnWall() && !IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-        }
-
+        
         Flip();
     }
 
     private void FixedUpdate()
     {
-        if (!IsOnWall())
-        {
-            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        } 
+        
     }
 
     private void Flip()
