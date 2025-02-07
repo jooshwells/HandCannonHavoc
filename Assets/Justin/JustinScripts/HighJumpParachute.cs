6c2f4fe6a98b40fe8379c46d3b2e27f32cca7077
justintong0906 <justintong0906@gmail.com>
2025-02-07 17:50:26 -0500

    Justin Startup

---
+++ Assets/Justin/JustinScripts/HighJumpParachute.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // psuedocode for later
+
+        /* if (!wallsliding)
+         * {
+         *      highjump and parachute activation:
+         *      if (movement ability button pressed)
+         *          propel player upwards like a high jump
+         *          then, animation of parachute coming out
+         *          parachuting = true
+         * } 
+         * 
+         * if(parachuting)
+         * {
+         *      gravity of player(?) set to low to simulate parachuting
+         *      (player can still move left and right while falling)
+         * }
+         * else
+         * {
+         *      gravity = (normal gravity)
+         * }
+         * 
+         * if(grounded || wallsliding)
+         * {
+         *      parachuting = false
+         * }
+         */
+    }
+}
