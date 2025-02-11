84936bd862321b1e5a9785aaae2c02cfa7bed8e5
jooshwells <joshwells637@gmail.com>
2025-02-11 14:41:48 -0500

    Working on grappling hook.

--- Assets/Josh/JoshScripts/GrapplingHook.cs
+++ Assets/Josh/JoshScripts/GrapplingHook.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class GrapplingHook : MonoBehaviour
 {
     private Transform parentTransform;
+    private bool firing = false;
+    private float angle;
+    private const bool compTool = true;
+    [SerializeField] private LayerMask grappleLayer;
+    [SerializeField] private float grappleSpeed = 8f;
+    [SerializeField] GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -15,41 +21,67 @@ public class GrapplingHook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         // Handle Positioning
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 parentPos = parentTransform.position;
-        parentPos = new Vector2(parentPos.x, parentPos.y + 0.5f);
+        if (!firing)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 parentPos = parentTransform.position;
+            parentPos = new Vector2(parentPos.x, parentPos.y + 0.5f);
 
-        Vector2 direction = mousePos - parentPos;
-        direction = direction.normalized;
+            Vector2 direction = mousePos - parentPos;
+            direction = direction.normalized;
 
-        Vector2 circlePosition = parentPos + direction * 2;
-        transform.position = circlePosition;
+            Vector2 circlePosition = parentPos + direction * 2;
+            transform.position = circlePosition;
 
-        // Handle Rotations
-        Vector2 dir = circlePosition - parentPos;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, 270f + angle);
+            // Handle Rotations
+            Vector2 dir = circlePosition - parentPos;
+            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, 270f + angle);
+        }
 
         // Lctrl
-        if(isActiveAndEnabled && Input.GetButtonDown("Fire1"))
+        if(isActiveAndEnabled && Input.GetMouseButtonDown(0))
         {
-            Debug.Log("test");
-            Fire();
+            Vector2 prevPos = transform.position;
+            firing = true;
+            //Debug.Log("firing");
+            StartCoroutine(Fire());
         }
+
 
     }
 
-    private void Fire() 
-    { 
-        // extend cursor with line until collision with terrain
+    //private void Fire() 
+    //{
+    //    // extend cursor with line until collision with terrain
+    //    while (!Physics2D.OverlapCircle(transform.position, 0.2f, grappleLayer))
+    //    {
+    //        Debug.Log("Doing stuff");
+    //        transform.position = new Vector2(transform.position.x + (float)(Mathf.Sin(45) * 0.5), transform.position.y + (float)(Mathf.Cos(45) * 0.5));
+    //    }
             
-        // if terrain is grappleable
-            // lock position to that terrain, create spring joint between two
-            // break spring joint on button release
-        // else if terrain not grappleable
-            // break grapple hook and nothing happens
+    //    // if terrain is grappleable
+    //        // lock position to that terrain, create spring joint between two
+    //        // break spring joint on button release
+    //    // else if terrain not grappleable
+    //        // break grapple hook and nothing happens
+
+    //}
+
+    IEnumerator Fire()
+    {
+        while (!Physics2D.OverlapCircle(transform.position, 0.2f, grappleLayer))
+        {
+            Vector2 direction = transform.up;
+            transform.position += (Vector3)(direction * grappleSpeed * Time.deltaTime);
+
+            yield return null; // Wait for the next frame
+        }
+
+        SpringJoint2D spring = player.GetComponent<SpringJoint2D>();
+        spring.enabled = compTool;
+        spring.connectedAnchor = transform.position;
 
     }
 
