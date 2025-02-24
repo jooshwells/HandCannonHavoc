c3bcb3b57dbbdf752b686374ffda8ebd2063cfa0
AndyW1016 <andyw101604@gmail.com>
2025-02-24 03:27:49 -0500

    finished basic shooting
    also added weapon sprites yippee

---
+++ Assets/Andy Stuff/Scripts/Shoot.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject bulletSprite;
+    [SerializeField] float bulletSpeed = 10f;
+    [SerializeField] float bulletDuration = 1f;
+
+    [SerializeField] Transform gunPos;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // left click
+        {
+            shoot();
+        }
+    }
+    void shoot()
+    {
+        GameObject bullet = Instantiate(bulletSprite, gunPos.position, gunPos.rotation);
+        SpriteRenderer bulletRenderer = bullet.GetComponent<SpriteRenderer>();
+        if (bulletRenderer != null)
+        {
+        bulletRenderer.enabled = true; // Make the bullet visible
+        }
+
+        Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - gunPos.position).normalized;
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        rb.velocity = direction * bulletSpeed;
+        flip(direction, bulletRenderer);
+        Destroy(bullet, bulletDuration);
+    }
+    
+    void flip(Vector2 dir, SpriteRenderer bullet)
+    {
+        Vector3 cur = bullet.transform.localScale;
+
+        if(dir.x > 0 && gunPos.localScale.y >0)
+        {
+            bullet.transform.localScale = new Vector3(cur.x, cur.y, cur.z);
+        }
+        if(dir.x<0 && gunPos.localScale.y >0) 
+        {
+            bullet.transform.localScale = new Vector3(-1*cur.x, cur.y, cur.z);
+        }
+        if(dir.x > 0 && gunPos.localScale.y <0)
+        {
+            bullet.transform.localScale = new Vector3(-1*cur.x,-1* cur.y, cur.z);
+        }
+        if(dir.x < 0 && gunPos.localScale.y <0)
+        {
+            bullet.transform.localScale = new Vector3(cur.x, -1*cur.y, cur.z);
+        }
+       
+    }
+}
