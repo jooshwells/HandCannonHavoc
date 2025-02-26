8fd8a3aa91e34edc719a832e5a176ba05f8368bd
justintong0906 <justintong0906@gmail.com>
2025-02-26 00:02:40 -0500

    alien scripting wip

--- Assets/Justin/JustinScripts/npcScript.cs
+++ Assets/Justin/JustinScripts/npcScript.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class NPC_Script : MonoBehaviour
 {
-    [SerializeField] private Transform player;
     private ProjectileScript projectile;
 
-    public float dist;
-    public float detectDist;
+    [SerializeField] private Transform player;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float detectDist;
+    [SerializeField] private float speed = 4f;
+
+    [SerializeField] private GameObject weapon;
+
+    private float dist;
+    private bool attacking;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +28,29 @@ public class NPC_Script : MonoBehaviour
     void Update()
     {
         dist = Vector2.Distance(player.position + new Vector3(0, 0.5f, 0), transform.position);
-        if (dist < detectDist) {
+        if (dist < detectDist)
+        {
+            attacking = true;
             attackPlayer();
         }
-
+        else
+        {
+            attacking = false;
+            stopAttacking();
+        }
     }
 
-    public void attackPlayer()
+    private void attackPlayer()
     {
+        
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        
+        //attacking animation, pull out gun
+        weapon.SetActive(true); //for now, just activate the gun object
+    }
 
+    private void stopAttacking()
+    {
+        weapon.SetActive(false);
     }
 }
