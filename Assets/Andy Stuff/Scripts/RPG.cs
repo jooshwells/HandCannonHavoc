6ec10ecfabed75015c41f038ad5a355aabb537d7
AndyW1016 <andyw101604@gmail.com>
2025-03-16 20:02:21 -0400

    RPG gun, adjusted bullet interpolations

---
+++ Assets/Andy Stuff/Scripts/RPG.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RPG : MonoBehaviour 
+{
+
+    private Transform player;
+    [SerializeField] GameObject bulletSprite;
+
+    [SerializeField] Transform gunPos;
+    [SerializeField] Transform gunBarrel;
+
+    [SerializeField] float bulletSpeed = 10f;
+    [SerializeField] float bulletDuration = 1f;
+    [SerializeField] float fireRate = .5f;
+    [SerializeField] float attackDamage = 50f;
+
+    private float nextBullet = 0f;
+
+    [SerializeField] int magSize = 1;
+    [SerializeField] float reloadSpeed = 2f;
+    [SerializeField] float recoil = 20f;
+
+    private int currentAmmo;
+    private bool isReloading = false;
+    private Rigidbody2D playerRb;
+
+
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmmo = magSize;
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Bullets"), LayerMask.NameToLayer("Bullets")); // prevent bullet collision
+        player =transform.parent.parent;
+        playerRb = transform.parent.parent.GetComponent<Rigidbody2D>();  //used for recoil
+    }
+
+    // reset ammo when swapping between guns
+    void OnEnable()
+    {
+        currentAmmo = magSize;
+        isReloading = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isReloading) return;
+
+        if(currentAmmo <=0 || (Input.GetKeyDown(KeyCode.R) && currentAmmo < magSize))
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        if (Input.GetMouseButton(0) && Time.time >= nextBullet) // left click
+        {
+            shoot();
+            nextBullet = Time.time +fireRate;
+        }
+    }
+    void shoot()
+    {
+        if(currentAmmo <=0) return;
+        currentAmmo--;
+
+
+        GameObject rpg = Instantiate(bulletSprite, gunBarrel.position, gunPos.rotation);    
+        SpriteRenderer bulletRenderer = rpg.GetComponent<SpriteRenderer>();
+        if (bulletRenderer != null)
+        {
+            bulletRenderer.enabled = true; // Make the bullet visible
+        }
+        RPGboomy bulletScript = rpg.GetComponent<RPGboomy>();
+        bulletScript.SetInstantiator(gameObject);
+        bulletScript.SetAttackDamage(attackDamage);
+
+
+        Vector2 direction = (gunPos.right).normalized;
+
+        // deal with gun firing inwards
+        if(player.transform.localScale.x <0) 
+        {
+            direction = -(gunPos.right).normalized;
+        }
+
+        Rigidbody2D rb = rpg.GetComponent<Rigidbody2D>();
+        rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+
+
+        rb.velocity = direction * bulletSpeed;
+        flip(direction, bulletRenderer);
+        Recoil(direction);
+        Destroy(rpg, bulletDuration);
+    }
+    
+    // spagetthi code for handling weird sprite flipping
+    void flip(Vector2 dir, SpriteRenderer bullet)
+    {
+        Vector3 cur = bullet.transform.localScale;
+
+        if(dir.x > 0 && gunPos.localScale.y >0)
+        {
+            bullet.transform.localScale = new Vector3(cur.x, cur.y, cur.z);
+        }
+        if(dir.x<0 && gunPos.localScale.y >0) 
+        {
+            bullet.transform.localScale = new Vector3(-1*cur.x, cur.y, cur.z);
+        }
+        if(dir.x > 0 && gunPos.localScale.y <0)
+        {
+            bullet.transform.localScale = new Vector3(-1*cur.x,-1* cur.y, cur.z);
+        }
+        if(dir.x < 0 && gunPos.localScale.y <0)
+        {
+            bullet.transform.localScale = new Vector3(cur.x, -1*cur.y, cur.z);
+        }
+       
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+    
+
+        yield return new WaitForSeconds(reloadSpeed);
+
+        currentAmmo = magSize;
+        isReloading = false;
+    }
+void Recoil(Vector2 shotDirection)
+    {
+        float horizontalRecoil = -shotDirection.x * recoil *.6f;
+        float verticalRecoil = -shotDirection.y * recoil;
+
+        playerRb.AddForce(new Vector2(horizontalRecoil,verticalRecoil), ForceMode2D.Impulse);
+    
+    }
+}
---
+++ Assets/Andy Stuff/Scripts/RPGboomy.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGboomy : MonoBehaviour
+{
+    private GameObject instantiator;
+    private float attackDamage;
+    private bool isExploding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetInstantiator(GameObject inst)
+    {
+        instantiator = inst;
+    }
+    public void SetAttackDamage(float damage)
+    {
+        attackDamage = damage;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Collided with: " + collision.gameObject.tag);
+
+        if (!collision.gameObject.CompareTag(instantiator.tag)) 
+        {
+            if (!isExploding)
+            {
+                isExploding = true;
+                //work on knockback later
+                // gameObject.GetComponentInChildren<BoxCollider2D>().enabled = true;
+                // gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                // gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+                StartCoroutine(Explode());
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // work on collision knockback for enemies
+        // Debug.Log("Trigger triggering");
+        // if (collision.gameObject.CompareTag("Player"))
+        // {
+        //     gameObject.GetComponentInChildren<BoxCollider2D>().enabled = true;
+        //     Vector2 dir = (transform.position - player.transform.position).normalized;
+        //     Debug.Log("Attempting to Force <" + (12*-dir.x) + "," + (16*-dir.y) + "> to " + rb.gameObject.ToString());
+        //     rb.gameObject.GetComponent<PlayerController>().KnockBack(new Vector2(12 * -dir.x, 16 * dir.y), 0.2f);
+        // }
+
+
+        // Health Update Event for Enemies
+        if (collision.CompareTag("Enemy"))
+        {
+            GameObject par = null;
+
+            if (!isExploding)
+            {
+                isExploding = true;
+                StartCoroutine(Explode());
+
+            }
+            if (collision.gameObject.transform.parent != null)
+            {
+                par = ((collision.gameObject).transform.parent).gameObject; // this gets the parent using transform
+            }
+
+            if(par != null)
+            {
+                par.GetComponentInChildren<EnemyHealthScript>().UpdateHealth(attackDamage); // if we have a parent, check its children for a script to update the health
+            } 
+            else
+            {
+                collision.gameObject.GetComponentInChildren<EnemyHealthScript>().UpdateHealth(attackDamage); // else just check the collision objects children for the script
+            }
+            Destroy(gameObject); // destroy the bullet
+        }
+        else if (!(collision.CompareTag(instantiator.tag))) {
+            {
+                if (!isExploding)
+                {
+                isExploding = true;
+                StartCoroutine(Explode());
+
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        Animator anim = gameObject.GetComponent<Animator>();
+
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        //anim.SetBool("isExploding", true);
+        anim.Play("Exploding");
+        float explosionDuration = anim.GetCurrentAnimatorStateInfo(0).length;
+        
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        gameObject.GetComponent<Rigidbody2D>().simulated = false;
+        
+        
+        yield return new WaitForSeconds(explosionDuration);
+        Destroy(gameObject);
+        isExploding=false;
+    }
+}
