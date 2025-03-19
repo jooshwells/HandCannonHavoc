eceb145402175facd49c3972e7357a4edf89176a
Joshua Wells <144137022+jooshwells@users.noreply.github.com>
2025-03-18 21:47:10 -0400

    Implemented DOT on red tiles, and made a simple slime enemy.

--- Assets/Josh/JoshScripts/DamageGeometryScript.cs
+++ Assets/Josh/JoshScripts/DamageGeometryScript.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class DamageGeometryScript : MonoBehaviour
 {
+    private bool playerTakingDamage = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Change to OnCollisionStay to ensure player takes DOT? 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerTakingDamage = true;
+            StartCoroutine(DamageOverTime(collision));
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerTakingDamage = false;
+        }
+    }
+
+    private IEnumerator DamageOverTime(Collision2D collision)
+    {
+        while (playerTakingDamage)
         {
             collision.gameObject.GetComponent<PlayerHealthScript>().Hit(10);
+            yield return new WaitForSeconds(1f);
         }
     }
 }
--- Assets/Josh/JoshScripts/PlayerHealthScript.cs
+++ Assets/Josh/JoshScripts/PlayerHealthScript.cs
@@ -9,14 +9,27 @@ public class PlayerHealthScript : MonoBehaviour
     private float hp = 100;
     public Image healthBar;
     private Vector3 originalScale;
+    private bool isInvincible = false;
+    [SerializeField] private float invincibilityTime = 1.5f;
 
     public void Hit(int damage)
     {
+        if (isInvincible)
+        {
+            return;
+        }
+
         GetComponent<PlayerHitEffect>().TakeDamage();
         hp -= damage;
         float healthPercent = Mathf.Clamp(hp / maxHp, 0f, 1f);
         healthBar.rectTransform.localScale = new Vector3(Mathf.Max(originalScale.x * healthPercent, 0f), originalScale.y, originalScale.z);
+    }
 
+    private IEnumerator InvincibilityTimer()
+    {
+        isInvincible = true;
+        yield return new WaitForSeconds(invincibilityTime);
+        isInvincible = false;
     }
 
     // Start is called before the first frame update
